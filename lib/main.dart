b4a15a3a62d0b622c6a235e674750e16fa679618
Nilupa Illangarathna <nilupalakshantha50@gmail.com>
2022-11-07 16:06:44 +0530

    all the rrest dart files are cleanrd and finalized

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,7 @@ import 'Pages/auth_path.dart';
 import 'Pages/Settings.dart';
 import 'Pages/navigation_home_screen_with_App_Drawer.dart';
 import 'Pages/PredictorPage.dart';
-import 'Test.dart';
 import 'Pages/OverViewScreen.dart';
-
-//README
-//CHECK
-//Assets ->fitnessapp folder
-//Datafiles -> CURRENTLY WORKING ON
-
 
 
 int GloableLoadingVariable=1;
@@ -32,7 +25,6 @@ Future main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({Key? key}) : super(key: key);
 
   var ColourList =ColorsClass();
 
@@ -47,27 +39,20 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
 
-
-      // home: OverViewScreen(),
-      //
-      //
       initialRoute: "fingerprint_page",
 
       routes: {
-        "testpage": (context) => Test(),
         "homepage": (context) => NavigationHomeScreen(),
         "incomes_page": (context) => IncomesPage(true),
         "expenses_page": (context) => ExpensesPage(false),
-        // "overview_page": (context) => OverviewPage(),
         "overview_page": (context) => OverViewScreen(),
         "transections_page": (context) => TransectionsList(),
         "budget_page": (context) => Explore(),
         "fingerprint_page": (context) => AuthPath(),
         "settings_page": (context) => SettingsPage(),
         "predictor_page": (context) => PredictorPage(),
-        // "introduction_page": (context) => IntroductionAnimationScreen(),
+
       },
-      // home: LogoRotate(),
     );
   }
 }
